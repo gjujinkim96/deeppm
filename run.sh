76d4f9e428cb13cfe3c7abecc41bfffcc4e0e152
rolypolyvg295 <22863750+rolypolyvg295@users.noreply.github.com>
2023-11-24 01:36:03 +0900

    Add configs

--- run.sh
+++ run.sh
@@ -35,8 +35,9 @@
 # python run_DeepPM.py --cfg config/deeppm-nt-relu.yaml --exp_name 'DeepPM+NT+relu' --exp_override 
 # python run_DeepPM.py --cfg config/deeppm-nt-dropout.yaml --exp_name 'DeepPM+NT+dropout' --exp_override 
 
-# python run_DeepPM.py --cfg config/power-deeppm-nt.yaml --exp_name 'Power+DeepPM+NT+Mapex100' --exp_override 
+python run_DeepPM.py --cfg config/power-deeppm-nt.yaml --exp_name 'Power+DeepPM+NT+Batch 8' --exp_override 
+# python run_DeepPM.py --cfg config/power-deeppm-nt-vessl.yaml --exp_name 'Power+DeepPM+NT+LR 0.00003' --exp_override 
 # python run_DeepPM.py --cfg config/power-lstm-nt.yaml --exp_name 'Power+LSTM+NT+Mapex100' --exp_override 
 
-python run_DeepPM.py --cfg config/power-deeppm.yaml --exp_name 'Power+DeepPM+Mapex100' --exp_override 
+# python run_DeepPM.py --cfg config/power-deeppm.yaml --exp_name 'Power+DeepPM+Mapex100' --exp_override 
 # python run_DeepPM.py --cfg config/power-lstm.yaml --exp_name 'Power+LSTM+Mapex100' --exp_override 
